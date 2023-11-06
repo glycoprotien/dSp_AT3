8fde10f6987060bb9001adc74b02d1638d4f330d
glycoprotien <1223759889@qq.com>
2023-11-07 00:39:14 +0800

    阿萨

--- Data-Explorer-Web-App-main/tab_num/logics.py
+++ Data-Explorer-Web-App-main/tab_num/logics.py
@@ -28,7 +28,7 @@ class NumericColumn:
   
     """
     def __init__(self, df):
-        # self.file_path = None
+        self.file_path = None
         self.df = df
         self.cols_list = []
         self.serie = None

0b76ae7e14c0090f9b882500014346e5b6aa0bcd
glycoprotien <1223759889@qq.com>
2023-11-06 23:58:32 +0800

    asd

--- Data-Explorer-Web-App-main/app/streamlit_app.py
+++ Data-Explorer-Web-App-main/app/streamlit_app.py
@@ -47,8 +47,8 @@ if st.session_state.file_path is not None:
     st.session_state.df = pd.read_csv(st.session_state.file_path)
     tab_df, tab_num, tab_text, tab_date = st.tabs(["DataFrame", "Numeric Serie", "Text Serie", "Datetime Serie"])
     with tab_df:
-        #display_tab_df_content(file_path=st.session_state.file_path)
-        pass
+        display_tab_df_content(file_path=st.session_state.file_path)
+        
     with tab_num:
         display_tab_num_content(df = st.session_state.df)
     with tab_text:

b267a3ce73489627ae4013c7b2150a0715cd6e8d
glycoprotien <1223759889@qq.com>
2023-11-07 14:03:23 +0800

    ads

--- Data-Explorer-Web-App-main/app/streamlit_app.py
+++ Data-Explorer-Web-App-main/app/streamlit_app.py
@@ -44,13 +44,13 @@ with st.expander("ℹ️ - Streamlit application for performing data exploration
 
 # If a CSV file is uploaded, display the different tabs
 if st.session_state.file_path is not None:
-    file_path=st.session_state.file_path
+    st.session_state.df=st.session_state.file_path
     tab_df, tab_num, tab_text, tab_date = st.tabs(["DataFrame", "Numeric Serie", "Text Serie", "Datetime Serie"])
     with tab_df:
         display_tab_df_content(file_path=st.session_state.file_path)
         pass
     with tab_num:
-        display_tab_num_content(df = pd.read_csv(st.session_state.file_path))
+        display_tab_num_content(df=st.session_state.df)
     with tab_text:
         pass
     with tab_date:
